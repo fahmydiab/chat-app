2526ad4c0b4af2c2af5fff291a63b60b8e1f309a
Fahmy Diab <king.diab.007@gmail.com>
2019-07-20 12:57:29 +0200

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,7 +15,15 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New user connected');
 
- 
+ socket.emit('newMessage',{
+     from: 'Admin',
+     text: 'welcome to chat app'
+ });
+ socket.broadcast.emit('newMessage',{
+     from: 'Admin',
+     text:'New User Joined',
+     createdAt: new Date().getTime()
+ });
 
   socket.on('createMessage',(message)=>{
     console.log('createMessage', message);
@@ -23,7 +31,14 @@ io.on('connection',(socket)=>{
         from: message.from,
         text: message.text,
         createdAt: new Date().getTime()
-    })
+    });
+    // socket.broadcast.emit(
+    //     'newMessage',{
+    //         from: message.from,
+    //         text: message.text,
+    //         createdAt: new Date().getTime()
+    //     }
+    // )
   });
 
 
