8734e87ef4c3c2df0904fcff0bef8e31e6d7140c
Fahmy Diab <king.diab.007@gmail.com>
2019-07-21 13:50:52 +0200

    Send Messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -69,7 +69,6 @@ function scrollToBottom (){
            
            var messageTextbox = jQuery('[name=message]');
            socket.emit('createMessage',{
-               from:'User',
                text:messageTextbox.val()
            }, function(){
              messageTextbox.val('')
