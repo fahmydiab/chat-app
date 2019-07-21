afffff63fb1dc467cf01b9544977c25c9efc52dc
Fahmy Diab <king.diab.007@gmail.com>
2019-07-21 11:00:12 +0200

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,17 @@
 
 var socket = io();
+function scrollToBottom (){
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop("scrollTop");
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
 
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }}
        socket.on('connect',function() {
            console.log('connected to server');
 
@@ -21,19 +32,20 @@ var socket = io();
         });
 
         jQuery('#messages').append(html);
+        scrollToBottom();
        });
 
        socket.on('newLocationMessage', function(message){
         var formattedTime = moment(message.createdAt).format('h:mm a');
         var template = jQuery('#location-message-template').html();
         var html = Mustache.render(template, {
-            url: message.url,
             from: message.from,
+            url: message.url,
             createdAt: formattedTime
         });
 
         jQuery('#messages').append(html);
-          
+          scrollToBottom();
        });
        
        jQuery('#message-form').on('submit', function(e){
